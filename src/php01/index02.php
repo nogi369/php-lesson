a3d08ec21f4b028d6a127571959d2fbef22f1abe
nogi <Createc369.Re@gmail.com>
2024-04-25 09:09:35 +0900

    add: 0-3

--- src/php01/index02.php
+++ src/php01/index02.php
@@ -1,8 +1,5 @@
 <?php
-const FELANGUAGE = "React.js";
-echo FELANGUAGE;
-// const FELANGUAGE = "Vue.js"
-// echo FELANGUAGE;
+
 // 変数
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // $item = "JavaScript";
@@ -10,11 +7,47 @@ echo FELANGUAGE;
 // $item = "PHP";
 // echo $item;
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
 // 定数
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-// const FELANGUAGE = "Vue.js"
+// const FELANGUAGE = "Vue.js";
 // echo FELANGUAGE;
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
+// データ型
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// インプット
+// $item = "PHP";
+// $item = 123;
+// echo $item;
+// アウトプット
+// const INTEGER = 20;
+// $item = "こんにちは";
+// echo INTEGER;
+// echo $item;
+// echo 20;
+// echo "こんにちは";
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+// 連結演算子
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// インプット
+// $name1 = "Sato" . "Taro";
+// $name2 = "Tanaka";
+// $last_name = "Yamada";
+// $first_name = "Saburo";
+
+// echo $name1;
+// echo "<br>";
+// echo $name2 . "Jiro";
+// echo "<br>";
+// echo $last_name . $first_name;
+// アウトプット
+$name = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+// TanakaJiro
+echo $name . "Jiro";
+echo "<br>";
+echo $last_name . $first_name;
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
