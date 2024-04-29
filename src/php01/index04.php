e0f5d9486ad34c4f8e9805db33ec3fe6dd92c22b
nogi <Createc369.Re@gmail.com>
2024-04-29 17:49:17 +0900

    add: 0-5 switch文 三項演算子

--- src/php01/index04.php
+++ src/php01/index04.php
@@ -67,14 +67,66 @@
 // }
 
 // 模範解答
-$a = 6;
+// $a = 7;
 
-if ($a === 5) {
-echo "\$aは5です";
-}elseif ($a === 7){
-echo "\$aは7です";
-}else{
-echo "\$aは5と7以外です";
-}
+// if ($a === 5) {
+// echo "\$aは5です";
+// }elseif ($a === 7){
+// echo "\$aは7です";
+// }else{
+// echo "\$aは5と7以外です";
+// }
 
 // ＝＝＝＝＝
+
+
+// switch文
+
+// ＝＝＝＝＝
+// Q. switch 文を使用して、結果が以下のようになるコードを記述しましょう。
+
+// base
+// $people = "Saburo";
+
+// switch ($people) {
+//   case "Ichiro":
+//     echo "Ichiroです";
+//     break;
+//   case "Jiro":
+//     echo "Jiroです";
+//     break;
+//   case "Saburo":
+//     echo "Saburoです";
+// }
+
+// 模範解答
+// $people = "Saburo";
+
+// switch ($people) {
+//   case "Taro":
+//     echo "太郎です";
+//     break;
+//   case "Jiro":
+//     echo "次郎です";
+//     break;
+//   case "Saburo":
+//     echo "三郎です";
+// }
+
+// ＝＝＝＝＝
+
+
+// 三項演算子
+
+// ＝＝＝＝＝
+// Q. 三項演算子を使用して、結果が以下のようになるコードを記述しましょう。
+
+// base
+// $a = 7;
+// $result = $a === 7 ? "TRUE" : "FALSE";
+// echo $result;
+
+// 模範解答
+$a = 7;
+$b = ($a > 5) ? "TRUE" : "FALSE";
+echo $b;
